4e8f6ac74219f8239bcd0f87276f0465e3816dbc
Iain <Iain@user-HP.(none)>
2014-07-14 22:38:00 +1000

    ltable changes

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 		</ul>
 	</nav>
 	<section id="left-sidebar">
-		<h2>Left Sidebar</h2>
-			<?php include("basicTable.html"); ?>
+		
+		<?php include("basicTable.html"); ?>
 		<article>
 			<!-- <div data-type=timetable data-id="30670" id="wgt-30670" class="tap-sport-tools" style="width:300px; height:auto;"></div>
 			<div id="wgt-ft-30670" style="width:296px;"><p>Football results provided by <a href="http://www.whatsthescore.com/football/england/premier-league/" target="_blank" rel="nofollow"><img src="http://medias.whatsthescore.com/upload/logo-s.png" alt="whatsthescore.com" /></a></p></div><style type="text/css">#wgt-ft-30670  {background:#FFFFFF !important;color:#484848 !important;text-decoration:none !important;padding:4px 2px !important;margin:0 !important;}#wgt-ft-30670 * {font:10px Arial !important;}#wgt-ft-30670 a {color:#484848 !important;}#wgt-ft-30670 img {vertical-align:bottom !important;height:15px !important;}</style><script type="text/javascript" src="http://tools.whatsthescore.com/load.min.js?219"></script> -->
@@ -55,7 +55,7 @@
 		vestibulum. Nam iaculis pulvinar eros, eu cursus metus rutrum sed.</p>
 	</section>
 	<section id="right-sidebar">
-		<h2>Right Sidebar</h2>
+		<?php include("basicTable.html"); ?>
 		<article>
 			<div style="width:290px; text-align:center; font-family:Verdana; font-size:11px;">
 			<iframe src="http://www.physioroom.com/affiliate/common/syndicate_table2_mini.php?w=270" width="290" height="2000" frameborder="0"></iframe>
